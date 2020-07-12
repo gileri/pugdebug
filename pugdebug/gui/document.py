04386a47c37946f4fba81bc8e64d004ea4320f4a
Andrey Moskalev <andreym333@gmail.com>
2020-07-12 10:11:43 +0300

    Draw line numbers in the code editor itself

--- pugdebug/gui/document.py
+++ pugdebug/gui/document.py
@@ -11,8 +11,8 @@ __author__ = "robertbasic"
 
 import math
 
-from PyQt5.QtCore import pyqtSignal, Qt, QRect
-from PyQt5.QtWidgets import (QWidget, QPlainTextEdit, QTextEdit, QGridLayout,
+from PyQt5.QtCore import pyqtSignal, Qt, QRect, QSize
+from PyQt5.QtWidgets import (QWidget, QPlainTextEdit, QTextEdit,
                              QShortcut, QInputDialog)
 from PyQt5.QtGui import (QColor, QTextFormat, QTextCursor, QPainter,
                          QTextBlockUserData, QFont, QKeySequence)
@@ -21,170 +21,23 @@ from pugdebug.syntaxer import PugdebugSyntaxer
 from pugdebug.models.settings import get_setting
 
 
-class PugdebugDocument(QWidget):
+class PugdebugDocument(QPlainTextEdit):
     """A document widget to display code and line numbers
 
-    This document widget is built from two different widgets, one being
-    a QPlainTextEdit widget that holds the contents of the document, and
-    the other being a simple QWidget that gets the line numbers painted on.
+    Line numbers is a separate widget that is rendered on the left margin
+    of the QPlainTextEdit viewport.
     """
-
-    document_contents = None
-
-    line_numbers = None
 
     document_double_clicked_signal = pyqtSignal(str, int)
 
     def __init__(self, document_model, formatter):
-        super(PugdebugDocument, self).__init__()
+        super().__init__()
 
-        # The QPlainTextEdit widget that holds the contents of the document
-        self.document_contents = PugdebugDocumentContents(
-            self,
-            document_model,
-            formatter
-        )
+        self.set_editor_features()
 
-        # The QWidget that gets the line numbers
         self.line_numbers = PugdebugLineNumbers(self)
-
-        self.document_contents.updateRequest.connect(
-            self.handle_document_contents_update_request
-        )
-
-        self.layout = QGridLayout(self)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.addWidget(self.line_numbers, 0, 0, 1, 1)
-        self.layout.addWidget(self.document_contents, 0, 1, 1, 1)
-
-        self.shortcut_search = QShortcut(QKeySequence("Ctrl+F"), self)
-        self.shortcut_search.activated.connect(self.show_search_modal)
-
-        self.shortcut_move_to_line = QShortcut(QKeySequence("Ctrl+G"), self)
-        self.shortcut_move_to_line.activated.connect(self.show_move_to_line)
-
-    def show_search_modal(self):
-        text, ok = QInputDialog.getText(self, 'Search', 'Insert word')
-        self.document_contents.find(text)
-
-    def show_move_to_line(self):
-        text, ok = QInputDialog.getText(self, 'Go To', 'Line number')
-        self.document_contents.move_to_line(int(text), True)
-
-    def handle_document_changed(self, document_model):
-        """Handle when a document gets changed
-
-        Set the new document contents.
-        """
-        self.document_contents.update_contents(document_model)
-
-    def handle_editor_features_changed(self):
-        self.document_contents.set_editor_features()
-        self.line_numbers.set_font()
-
-    def handle_document_contents_update_request(self, rect, dy):
-        """Handle the update request for document contents
-
-        Scroll the line numbers along with the document contents.
-
-        Set the width of the line numbers widget.
-        """
-        if (dy != 0):
-            self.line_numbers.scroll(0, dy)
-
-        line_count = self.document_contents.blockCount()
-        self.line_numbers.set_line_count(line_count)
-
-    def paint_line_numbers(self, line_numbers, event):
-        """Paint the line numbers
-
-        For every visible block in the document draw it's corresponding line
-        number in the line numbers widget.
-        """
-        font_metrics = self.document_contents.fontMetrics()
-
-        painter = QPainter(line_numbers)
-
-        block = self.document_contents.firstVisibleBlock()
-        line_number = block.blockNumber()
-
-        height = font_metrics.height()
-        width = line_numbers.width()
-        breakpoint_size = 8
-        breakpoint_y_offset = height / 2 - breakpoint_size / 2
-
-        while block.isValid():
-            # blocks are numbered from zero
-            line_number += 1
-
-            content_offset = self.document_contents.contentOffset()
-            # Get the top coordinate of the current block
-            # to know where to paint the line number for it
-            block_top = (self.document_contents
-                             .blockBoundingGeometry(block)
-                             .translated(content_offset)
-                             .top())
-
-            if not block.isVisible() or block_top >= event.rect().bottom():
-                break
-
-            # If block has a breakpoint,
-            # draw a green rectangle by the line number
-            # if the block number matches the current line number
-            # make it red, as it is then a breakpoint hit
-            if self.document_contents.block_has_breakpoint(block):
-                brush = painter.brush()
-                brush.setStyle(Qt.SolidPattern)
-                if self.document_contents.block_is_current(block):
-                    brush.setColor(Qt.red)
-                else:
-                    brush.setColor(Qt.darkGreen)
-                painter.setBrush(brush)
-                rect = QRect(0, block_top + breakpoint_y_offset,
-                             breakpoint_size, breakpoint_size)
-                painter.drawRect(rect)
-
-            # Convert the line number to string so we can paint it
-            text = str(line_number)
-
-            painter.drawText(0, block_top, width, height, Qt.AlignRight, text)
-
-            block = block.next()
-
-        painter.end()
-
-    def get_path(self):
-        return self.document_contents.document_model.path
-
-    def move_to_line(self, line, is_current=True):
-        self.document_contents.move_to_line(line, is_current)
-        self.rehighlight_breakpoint_lines()
-
-    def remove_line_highlights(self):
-        self.document_contents.remove_line_highlights()
-        self.rehighlight_breakpoint_lines()
-
-    def rehighlight_breakpoint_lines(self):
-        """Rehighlight breakpoint lines
-
-        Update the line numbers so the paint event gets fired
-        and so the breakpoints get repainted as well.
-        """
-        self.line_numbers.update()
-
-
-class PugdebugDocumentContents(QPlainTextEdit):
-
-    document_widget = None
-
-    document_model = None
-
-    syntaxer = None
-
-    def __init__(self, document_widget, document_model, formatter):
-        super(PugdebugDocumentContents, self).__init__()
-
-        self.document_widget = document_widget
+        self.blockCountChanged.connect(self.update_line_numbers_width)
+        self.updateRequest.connect(self.update_line_numbers)
 
         self.document_model = document_model
 
@@ -200,18 +53,11 @@ class PugdebugDocumentContents(QPlainTextEdit):
 
         self.viewport().setCursor(Qt.ArrowCursor)
 
-        self.set_editor_features()
+        self.shortcut_search = QShortcut(QKeySequence("Ctrl+F"), self)
+        self.shortcut_search.activated.connect(self.show_search_modal)
 
-    def update_contents(self, document_model):
-        """Update the contents of the document
-
-        Set the new contents of the document.
-
-        Refresh the syntaxer.
-        """
-        self.setPlainText(document_model.contents)
-        self.syntaxer.setDocument(self.document())
-        self.syntaxer.highlight()
+        self.shortcut_move_to_line = QShortcut(QKeySequence("Ctrl+G"), self)
+        self.shortcut_move_to_line.activated.connect(self.show_move_to_line)
 
     def set_editor_features(self):
         font = QFont(get_setting('editor/font_family'))
@@ -225,6 +71,91 @@ class PugdebugDocumentContents(QPlainTextEdit):
                     if get_setting('editor/enable_text_wrapping')
                     else QPlainTextEdit.NoWrap)
         self.setLineWrapMode(wrapMode)
+
+    def line_numbers_width(self):
+        digits = math.floor(math.log10(self.blockCount()) + 1)
+        return (self.line_numbers.padding_left +
+                digits * self.fontMetrics().width('0') +
+                self.line_numbers.padding_right)
+
+    def update_line_numbers_width(self, new_block_count=0):
+        self.setViewportMargins(self.line_numbers_width(), 0, 0, 0)
+
+    def update_line_numbers(self, rect, dy):
+        if dy:
+            self.line_numbers.scroll(0, dy)
+        else:
+            self.line_numbers.update(0, rect.y(),
+                                     self.line_numbers.width(),
+                                     rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.update_line_numbers_width()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        cr = self.contentsRect()
+        self.line_numbers.setGeometry(QRect(cr.x(), cr.y(),
+                                            self.line_numbers_width(),
+                                            cr.height()))
+
+    def paint_line_numbers(self, event):
+        """Paint the line numbers
+
+        For every visible block in the document draw it's corresponding line
+        number in the line numbers widget.
+        """
+        painter = QPainter(self.line_numbers)
+        painter.fillRect(event.rect(), Qt.lightGray)
+
+        block = self.firstVisibleBlock()
+        block_number = block.blockNumber()
+
+        content_offset = self.contentOffset()
+
+        number_width = (self.line_numbers.width() -
+                        self.line_numbers.padding_right)
+        number_height = self.fontMetrics().height()
+
+        breakpoint_size = 8
+        breakpoint_y_offset = number_height / 2 - breakpoint_size / 2
+
+        while block.isValid():
+            # Get the top coordinate of the current block
+            # to know where to paint the line number for it
+            block_top = (self.blockBoundingGeometry(block)
+                             .translated(content_offset)
+                             .top())
+
+            if not block.isVisible() or block_top > event.rect().bottom():
+                break
+
+            # If block has a breakpoint,
+            # draw a green rectangle by the line number
+            # if the block number matches the current line number
+            # make it red, as it is then a breakpoint hit
+            if self.block_has_breakpoint(block):
+                brush = painter.brush()
+                brush.setStyle(Qt.SolidPattern)
+                if self.block_is_current(block):
+                    brush.setColor(Qt.red)
+                else:
+                    brush.setColor(Qt.darkGreen)
+                painter.setBrush(brush)
+                painter.drawRect(QRect(0, block_top + breakpoint_y_offset,
+                                       breakpoint_size, breakpoint_size))
+
+            number_str = str(block_number + 1)
+            painter.setPen(Qt.black)
+            painter.drawText(0, block_top,
+                             number_width, number_height,
+                             Qt.AlignRight, number_str)
+
+            block = block.next()
+            block_number += 1
+
+        painter.end()
 
     def mousePressEvent(self, event):
         pass
@@ -248,19 +179,13 @@ class PugdebugDocumentContents(QPlainTextEdit):
 
         line_number = block.blockNumber() + 1
 
-        self.document_widget.document_double_clicked_signal.emit(
-            path,
-            line_number
-        )
+        self.document_double_clicked_signal.emit(path, line_number)
 
     def contextMenuEvent(self, event):
         pass
 
     def move_to_line(self, line, is_current=True):
-        """Move cursor to line
-
-        Move the cursor of the QPlainTextEdit that holds the document
-        contents to the line.
+        """Move cursor to specified line
 
         Move the cursor block by block until the block number matches
         the line number.
@@ -296,6 +221,8 @@ class PugdebugDocumentContents(QPlainTextEdit):
 
         self.setTextCursor(cursor)
 
+        self.rehighlight_breakpoint_lines()
+
     def highlight(self):
         selection = QTextEdit.ExtraSelection()
 
@@ -315,8 +242,12 @@ class PugdebugDocumentContents(QPlainTextEdit):
         Move the cursor to first (zero) line.
 
         Clear the extra selections in the file.
+
+        Rehighlight breakpoint lines.
         """
         self.setExtraSelections([])
+
+        self.rehighlight_breakpoint_lines()
 
     def block_has_breakpoint(self, block):
         user_data = self.__get_block_user_data(block)
@@ -347,39 +278,57 @@ class PugdebugDocumentContents(QPlainTextEdit):
             user_data = PugdebugBlockData()
         return user_data
 
+    def show_search_modal(self):
+        text, ok = QInputDialog.getText(self, 'Search', 'Insert word')
+        self.find(text)
+
+    def show_move_to_line(self):
+        text, ok = QInputDialog.getText(self, 'Go To', 'Line number')
+        self.move_to_line(int(text), True)
+
+    def handle_document_changed(self, document_model):
+        """Handle when a document gets changed
+
+        Set the new contents of the document.
+
+        Refresh the syntaxer.
+        """
+        self.setPlainText(document_model.contents)
+
+        self.syntaxer.setDocument(self.document())
+        self.syntaxer.highlight()
+
+    def handle_editor_features_changed(self):
+        self.set_editor_features()
+
+    def get_path(self):
+        return self.document_model.path
+
+    def rehighlight_breakpoint_lines(self):
+        """Rehighlight breakpoint lines
+
+        Update the line numbers so the paint event gets fired
+        and so the breakpoints get repainted as well.
+        """
+        self.line_numbers.update()
+
 
 class PugdebugLineNumbers(QWidget):
 
-    document_widget = None
+    def __init__(self, document):
+        super().__init__(document)
 
-    line_count = 1
+        self.document = document
 
-    def __init__(self, document_widget):
-        super(PugdebugLineNumbers, self).__init__()
+        # additional space before and after number
+        self.padding_left = 14
+        self.padding_right = 5
 
-        self.document_widget = document_widget
-
-        self.set_font()
-
-    def set_font(self):
-        font = QFont(get_setting('editor/font_family'))
-        font.setPointSize(get_setting('editor/font_size'))
-        self.setFont(font)
-
-        self.__set_width()
-
-    def set_line_count(self, line_count):
-        self.line_count = line_count
-        self.__set_width()
-
-    def __set_width(self):
-        digits = math.floor(math.log10(self.line_count) + 1)
-        # add 14 to have space to paint the breakpoint markers
-        width = digits * self.fontMetrics().width('0') + 14
-        self.setFixedWidth(width)
+    def sizeHint(self):
+        return QSize(self.document.line_numbers_width(), 0)
 
     def paintEvent(self, event):
-        self.document_widget.paint_line_numbers(self, event)
+        self.document.paint_line_numbers(event)
 
 
 class PugdebugBlockData(QTextBlockUserData):
